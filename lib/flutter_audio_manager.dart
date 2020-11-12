a6f3ff7add3021715e80c55b7b29061f9ce191a4
Samir Thapa <samir@nextbase.co.uk>
2020-11-12 12:02:21 +0000

    Remove static method calls

--- lib/flutter_audio_manager.dart
+++ lib/flutter_audio_manager.dart
@@ -38,17 +38,17 @@ class FlutterAudioManager {
       const MethodChannel('flutter_audio_manager');
   static void Function() _onInputChanged;
 
-  static Future<String> get platformVersion async {
+  Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<AudioInput> getCurrentOutput() async {
+  Future<AudioInput> getCurrentOutput() async {
     final List<dynamic> data = await _channel.invokeMethod('getCurrentOutput');
     return AudioInput(data[0], int.parse(data[1]));
   }
 
-  static Future<List<AudioInput>> getAvailableInputs() async {
+  Future<List<AudioInput>> getAvailableInputs() async {
     final List<dynamic> list =
         await _channel.invokeMethod('getAvailableInputs');
 
@@ -59,28 +59,28 @@ class FlutterAudioManager {
     return arr;
   }
 
-  static Future<bool> changeToSpeaker() async {
+  Future<bool> changeToSpeaker() async {
     return await _channel.invokeMethod('changeToSpeaker');
   }
 
-  static Future<bool> changeToReceiver() async {
+  Future<bool> changeToReceiver() async {
     return await _channel.invokeMethod('changeToReceiver');
   }
 
-  static Future<bool> changeToHeadphones() async {
+  Future<bool> changeToHeadphones() async {
     return await _channel.invokeMethod('changeToHeadphones');
   }
 
-  static Future<bool> changeToBluetooth() async {
+  Future<bool> changeToBluetooth() async {
     return await _channel.invokeMethod('changeToBluetooth');
   }
 
-  static void setListener(void Function() onInputChanged) {
+  void setListener(void Function() onInputChanged) {
     FlutterAudioManager._onInputChanged = onInputChanged;
     _channel.setMethodCallHandler(_methodHandle);
   }
 
-  static Future<void> _methodHandle(MethodCall call) async {
+  Future<void> _methodHandle(MethodCall call) async {
     if (_onInputChanged == null) return;
     switch (call.method) {
       case "inputChanged":
